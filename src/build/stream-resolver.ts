f28a234316626e0a352a0b3c88e008c1e36cf94a
Justin Fagnani <justinfagnani@google.com>
2016-05-03 03:43:03 -0700

    Fix handling of absolute URLs in StreamResolver Get the file from disk if it was not a source file in vinyl stream

--- src/build/stream-resolver.ts
+++ src/build/stream-resolver.ts
@@ -16,90 +16,131 @@ import {Transform} from 'stream';
 import File = require('vinyl');
 import * as url from 'url';
 
+export interface FileGetter {
+  (filePath: string, deferred: Deferred<string>): void;
+}
+
 export interface Config {
   entrypoint?: string;
+
   /**
    * Hostname to match for absolute urls.
    * Matches "/" by default
    */
   host?: string;
+
   /**
    * Prefix directory for components in url. Defaults to "/".
    */
   basePath?: string;
+
   /**
    * Filesystem root to search. Defaults to the current working directory.
    */
   root?: string;
+
   /**
    * Where to redirect lookups to siblings.
    */
   redirect?: string;
+  sources?: string[];
+  getFile?: FileGetter;
 }
 
 export class StreamResolver extends Transform /* implements Resolver*/ {
 
   _files = new Map<string, File>();
+
+  // filename -> Hydrolysis Deferred request for file
   _deferreds = new Map<string, Deferred<string>>();
-  config: Config;
+
+  host: string;
+  base: string;
+  root: string;
+  redirect: string;
+  entrypoint: string;
+  sources: string[];
+  getFile: FileGetter;
 
   constructor(config: Config) {
     super({objectMode: true});
-    this.config = config || {};
+    this.host = config.host;
+    this.base = config.basePath && decodeURIComponent(config.basePath);
+    this.root = config.root && path.normalize(config.root);
+    this.redirect = config.redirect;
+    if (!config.entrypoint) {
+      throw new Error('entrypoint must be specified');
+    }
+    this.entrypoint = config.entrypoint;
+    this.sources = config.sources || [];
+    this.getFile = config.getFile || getFile;
   }
 
   _transform(file: File, encoding: string, callback: (error?, data?) => void): void {
-    let isEntrypoint = minimatch(file.path, this.config.entrypoint, {
+    let isEntrypoint = minimatch(file.path, this.entrypoint, {
       matchBase: true,
     });
+
+
+    // If this is the entrypoint, pass the file on
     if (isEntrypoint) {
       this._files.set(file.path, file);
       callback(null, file);
       return;
     }
+
+    // If not, first see if the file has been requested...
     let deferred = this._deferreds.get(file.path);
     if (deferred) {
-      this._deferreds.delete(file.path);
+      // ... if so, resolve the request
       deferred.resolve(file.contents.toString());
+      this._deferreds.delete(file.path);
     } else {
+      // ... if not, store the file for a future request
       this._files.set(file.path, file);
     }
+    // Never pass on non-entrypoints
+    // TODO(justinfagnani): possibly change this after more testing
     callback(null, null);
   }
 
   accept(uri: string, deferred: Deferred<string>): boolean {
     var parsed = url.parse(uri);
-    var host = this.config.host;
-    var base = this.config.basePath && decodeURIComponent(this.config.basePath);
-    var root = this.config.root && path.normalize(this.config.root);
-    var redirect = this.config.redirect;
-
     var local: string;
 
-    if (!parsed.hostname || parsed.hostname === host) {
+    if (!parsed.hostname || parsed.hostname === this.host) {
       local = parsed.pathname;
     }
+
     if (local) {
       // un-escape HTML escapes
       local = decodeURIComponent(local);
 
-      if (base) {
-        local = path.relative(base, local);
-      }
-      if (root) {
-        local = path.join(root, local);
+      // If there's a base path, strip it from the file path, unless the file
+      // path is an absolute URL not under base, such as /bower_components/...
+      if (this.base && (!path.isAbsolute(local) || local.startsWith(this.base))) {
+        local = path.relative(this.base, local);
       }
 
-      if (redirect && isSiblingOrAunt(root, local)) {
-        local = redirectSibling(root, local, redirect);
-        getFile(local, deferred);
+      if (this.root) {
+        local = path.join(this.root, local);
+      }
+
+      let isInSources = this.sources.some(s => minimatch(local, path.join(this.root, s)));
+
+      // If the requested file is not in source, it will not be streamed into
+      // the resolver, so load it from disk
+      if (!isInSources) {
+        this.getFile(local, deferred);
         return true;
       }
 
-      let file : File = this._files.get(local);
+      let file: File = this._files.get(local);
       if (file) {
+        // If we have a file from the stream already, resolve it...
         deferred.resolve(file.contents.toString());
       } else {
+        // Otherwise save the deffered to resolve later
         this._deferreds.set(local, deferred);
       }
       return true;
@@ -108,9 +149,10 @@ export class StreamResolver extends Transform /* implements Resolver*/ {
     return false;
   }
 
-  complete() {
-    for (let deferred in this._deferreds.values) {
-      (<Deferred<string>><any>deferred).reject(null);
+  end() {
+    // When the stream is done, reject all non-resolved requests for files
+    for (let deferred of this._deferreds.values()) {
+      deferred.reject(null);
     }
   }
 }
