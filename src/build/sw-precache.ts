176fac7e678a76ee99eca4a6807c9411cb1fbc21
Daniel Freedman <compismyrx@gmail.com>
2016-05-10 16:07:15 -0700

    Fix precaching to be consistent (#76)
    * Fix precaching to be consistent
    
    sw-precache reads directly from disk to generate precache manifests, so
    we must wait for the streams to be complete before calling
    generateServiceWorker.
    
    Read the config file once outside of the stream, let sw-precache write
    to disk
    
    Clone config so that bundled and unbundled don't write into each other
    
    Fixes #75
    
    * Add tests for service worker generation
    
    Also handle uncaught promises in build
    Remove console.log
    
    * Switch out tmp for temp

--- src/build/sw-precache.ts
+++ src/build/sw-precache.ts
@@ -10,14 +10,14 @@
 
 import * as fs from 'fs';
 import * as path from 'path';
-import {Transform} from 'stream';
+import {PassThrough} from 'stream';
 import File = require('vinyl');
 
 // non-ES compatible modules
 const swPrecache = require('sw-precache');
 const Module = require('module');
 
-interface SWConfig {
+export interface SWConfig {
   cacheId?: string,
   directoryIndex?: string;
   dynamicUrlToDependencies?: {
@@ -47,40 +47,48 @@ interface SWConfig {
   verbose?: boolean;
 }
 
-function generateServiceWorker(
-    root: string,
-    main: string,
-    deps: string[],
-    buildRoot: string,
-    swConfig?: SWConfig
-  ): Promise<string> {
-  swConfig = swConfig || <SWConfig>{};
+export function generateServiceWorker(options: generateServiceWorkerOptions)
+: Promise<void> {
+  let swConfig = options.swConfig || <SWConfig>{};
   // strip root prefix, so buildRoot prefix can be added safely
-  deps = deps.map((p) => {
-    if (p.startsWith(root)) {
-      return p.substring(root.length);
+  let deps = options.deps.map((p) => {
+    if (p.startsWith(options.root)) {
+      return p.substring(options.root.length);
     }
     return p;
   });
-  let mainHtml = main.substring(root.length);
+  let mainHtml = options.main.substring(options.root.length);
   let precacheFiles = new Set(swConfig.staticFileGlobs);
   deps.forEach((p) => precacheFiles.add(p));
   precacheFiles.add(mainHtml);
 
   let precacheList = Array.from(precacheFiles);
-  precacheList = precacheList.map((p) => path.join(buildRoot, p));
+  precacheList = precacheList.map((p) => path.join(options.buildRoot, p));
 
   // swPrecache will determine the right urls by stripping buildRoot
-  swConfig.stripPrefix = buildRoot;
+  swConfig.stripPrefix = options.buildRoot;
   // static files will be pre-cached
   swConfig.staticFileGlobs = precacheList;
 
-  console.log(`Generating service worker for ${buildRoot}`);
+  console.log(`Generating service worker for ${options.buildRoot}`);
 
-  return swPrecache.generate(swConfig);
+  return swPrecache.write(options.serviceWorkerPath, swConfig);
 }
 
-export interface SWPreCacheTransformOptions {
+export function parsePreCacheConfig(configFile: string): Promise<SWConfig> {
+  return new Promise<SWConfig>((resolve, reject) => {
+    try {
+      let config: SWConfig = require(configFile);
+      resolve(config);
+    } catch(e) {
+      console.error(`Could not load sw-precache config from ${configFile}`);
+      console.error(e);
+      resolve();
+    }
+  });
+}
+
+export interface generateServiceWorkerOptions {
   /**
    * folder containing files to be served by the service worker.
    */
@@ -94,100 +102,16 @@ export interface SWPreCacheTransformOptions {
    */
   buildRoot: string;
   /**
-   * Name of the output service worker file.
+   * File path of the output service worker file.
    */
-  serviceWorkerName: string;
+  serviceWorkerPath: string;
   /**
-   * Promise that returns the list of files to be cached by the service worker.
-   *
-   * If not given, all files streamed to SWPreCacheTransform will be put into
-   * the precache list, except for the file matching `configFileName`.
+   * List of files to be cached by the service worker,
+   * in addition to files found in `swConfig.staticFileGlobs`
    */
-  deps?: Promise<string[]>;
+  deps: string[];
   /**
-   * Existing config file to use as a base for the serivce worker generation.
-   *
-   * This file will not be copied into the output bundles.
+   * Existing config to use as a base for the serivce worker generation.
    */
-  configFileName?: string;
-}
-
-export class SWPreCacheTransform extends Transform {
-  swConfig: SWConfig;
-  options: SWPreCacheTransformOptions;
-  fileSet: Set<string>;
-  fullConfigFilePath: string;
-
-  constructor(options: SWPreCacheTransformOptions) {
-    super({objectMode: true});
-    this.options = options;
-    // if no given deps, collect all input files as deps
-    if (!options.deps) {
-      this.fileSet = new Set<string>();
-    }
-    if (options.configFileName) {
-      this.fullConfigFilePath = path.resolve(
-        options.root,
-        options.configFileName
-      );
-    }
-  }
-
-  _transform(file: File, encoding: string, callback: (error?, data?) => void): void {
-    if (file.path === this.fullConfigFilePath) {
-      try {
-        if (file.path.endsWith('js')) {
-          // `module._compile` is the heart of `require`
-          // http://fredkschott.com/post/2014/06/require-and-the-module-system/
-          let m = new Module(file.path);
-          m._compile(
-            file.contents.toString(),
-            file.path
-          );
-          this.swConfig = m.exports;
-        } else if (file.path.endsWith('json')) {
-          this.swConfig = JSON.parse(file.contents.toString());
-        }
-      } catch(e) {
-        let cfn = this.options.configFileName;
-        console.error(`Could not load service worker config from ${cfn}`);
-        console.error(e);
-      }
-      callback();
-    } else {
-      if (this.fileSet) {
-        this.fileSet.add(file.path);
-      }
-      callback(null, file);
-    }
-  }
-
-  _flush(callback: (error?) => void) {
-    let promise: Promise<string[]>;
-    if (this.fileSet) {
-      promise = Promise.resolve(Array.from(this.fileSet));
-    } else {
-      promise = this.options.deps;
-    }
-    promise.then((deps) => {
-      return generateServiceWorker(
-        this.options.root,
-        this.options.main,
-        deps,
-        this.options.buildRoot,
-        this.swConfig
-      );
-    })
-    .then((config) => {
-      let file = new File({
-        path: path.resolve(
-          this.options.root,
-          this.options.serviceWorkerName
-        ),
-        contents: new Buffer(config)
-      });
-      this.push(file);
-      callback();
-    });
-  }
+  swConfig?: SWConfig;
 }
---
+++ test/build/precache_test.js
@@ -0,0 +1,84 @@
+/**
+ * @license
+ * Copyright (c) 2016 The Polymer Project Authors. All rights reserved.
+ * This code may only be used under the BSD style license found at http://polymer.github.io/LICENSE.txt
+ * The complete set of authors may be found at http://polymer.github.io/AUTHORS.txt
+ * The complete set of contributors may be found at http://polymer.github.io/CONTRIBUTORS.txt
+ * Code distributed by Google as part of the polymer project is also
+ * subject to an additional IP rights grant found at http://polymer.github.io/PATENTS.txt
+ */
+
+'use strict';
+
+const assert = require('chai').assert;
+const fs = require('fs');
+const path = require('path');
+const temp = require('temp').track();
+const vfs = require('vinyl-fs-fake');
+
+const precache = require('../../lib/build/sw-precache');
+
+suite('sw-precache', () => {
+  const configFile = path.resolve(__dirname, 'precache', 'config.js');
+  suite('parsing', () => {
+    test('js file', (done) => {
+      precache.parsePreCacheConfig(configFile).then((config) => {
+        assert.ok(config);
+        assert.property(config, 'staticFileGlobs');
+        done();
+      })
+    });
+  });
+
+  suite('generation', () => {
+    let buildRoot;
+    setup((done) => {
+      temp.mkdir('polymer-cli', (err, dir) => {
+          if (err) {
+            return done(err);
+          }
+          buildRoot = dir;
+          vfs.src(path.join(__dirname, 'precache/static/*'))
+          .pipe(vfs.dest(dir))
+          .on('finish', () => done());
+        }
+      );
+    });
+
+    teardown((done) => {
+      temp.cleanup(done)
+    });
+
+    test('without config', (done) => {
+      precache.generateServiceWorker({
+        root: path.resolve(__dirname, 'precache/static'),
+        main: path.resolve(__dirname, 'precache/static/fizz.html'),
+        buildRoot,
+        deps: [],
+        serviceWorkerPath: path.join(buildRoot, 'service-worker.js')
+      }).then(() => {
+        let content = fs.readFileSync(path.join(buildRoot, 'service-worker.js'), 'utf-8');
+        assert.include(content, '/fizz.html', 'main file should be present');
+        done();
+      });
+    });
+
+    test('with config', (done) => {
+      precache.parsePreCacheConfig(configFile).then((config) => {
+        return precache.generateServiceWorker({
+          root: path.resolve(__dirname, 'precache/static'),
+          main: path.resolve(__dirname, 'precache/static/fizz.html'),
+          buildRoot,
+          deps: [],
+          swConfig: config,
+          serviceWorkerPath: path.join(buildRoot, 'service-worker.js')
+        });
+      }).then(() => {
+        let content = fs.readFileSync(path.join(buildRoot, 'service-worker.js'), 'utf-8');
+        assert.include(content, '/fizz.html', 'main file should be present');
+        assert.include(content, '/foo.js', 'staticFileGlobs should match foo.js');
+        done();
+      });
+    });
+  })
+});
