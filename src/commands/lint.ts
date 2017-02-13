3943d4da2175c86be99606f7ce5f1933c5a304c2
Peter Burns <rictic@google.com>
2017-02-13 09:11:52 -0800

    Add more todos, hook up a couple command line args.

--- src/commands/lint.ts
+++ src/commands/lint.ts
@@ -17,6 +17,7 @@
 // unused code. Any imports that are only used as types will be removed from the
 // output JS and so not result in a require() statement.
 
+import {ArgDescriptor} from 'command-line-args';
 import * as logging from 'plylog';
 import {Analyzer} from 'polymer-analyzer';
 import {FSUrlLoader} from 'polymer-analyzer/lib/url-loader/fs-url-loader';
@@ -27,24 +28,66 @@ import {WarningPrinter} from 'polymer-analyzer/lib/warning/warning-printer';
 import * as lintLibTypeOnly from 'polymer-linter';
 import {ProjectConfig} from 'polymer-project-config';
 
-import {Command, CommandOptions} from './command';
+import {Command} from './command';
 
 const logger = logging.getLogger('cli.lint');
+
+export interface Options {
+  rules?: string[];
+  input?: string[];
+}
 
 export class LintCommand implements Command {
   name = 'lint';
 
-  description = 'Lints the project';
+  description = 'Scans';
 
-  args = [];
+  args: ArgDescriptor[] = [
+    {
+      name: 'input',
+      type: String,
+      alias: 'i',
+      defaultOption: true,
+      multiple: true,
+      description: 'Files to lint. If given, these files will be the only ' +
+          'ones linted, otherwise all files in the project will be linted.'
+    },
+    {
+      name: 'rules',
+      type: String,
+      alias: 'r',
+      multiple: true,
+      description: 'The lint rules/rule collections to apply. ' +
+          'See `polymer help lint` for a list of rules.',
+    }
+  ];
 
-  async run(_options: CommandOptions, config: ProjectConfig): Promise<any> {
+  /**
+   * TODO(rictic): things to make configurable:
+   *   - lint warning verbosity
+   *   - whether to use color (also: can we autodetect if color is supported?)
+   *   - add option for input files to polymer.json
+   *   - modules to load that can register new rules
+   *   - --watch
+   *   - --fix
+   */
+
+  /**
+   * TODO(rictic): expose rules and rulecollections off the lint registry so
+   *   that we can print their descriptions in the help menu.
+   */
+
+  async run(options: Options, config: ProjectConfig): Promise<any> {
     // Defer dependency loading until this specific command is run.
     const lintLib: typeof lintLibTypeOnly = require('polymer-linter');
 
-    const lintOptions = config.lint;
-    if (!lintOptions) {
-      logger.warn(`linting is configured through polymer.json. e.g.:
+    const lintOptions: Partial<typeof config.lint> = (config.lint || {});
+
+    const ruleCodes = options.rules || lintOptions.rules;
+    if (ruleCodes === undefined) {
+      logger.warn(
+          `You must state which lint rules to use. You can use --rules, ` +
+          `but for a project it's best to use polymer.json. e.g.
 
 {
   "lint": {
@@ -54,7 +97,7 @@ export class LintCommand implements Command {
       return;
     }
 
-    const rules = lintLib.registry.getRules(lintOptions.rules);
+    const rules = lintLib.registry.getRules(ruleCodes || lintOptions.rules);
     const filter = new WarningFilter({
       warningCodesToIgnore: new Set(lintOptions.ignoreWarnings || []),
       minimumSeverity: Severity.WARNING
@@ -68,16 +111,25 @@ export class LintCommand implements Command {
 
     const linter = new lintLib.Linter(rules, analyzer);
 
-    const warnings = await linter.lintPackage();
+    const warnings = await this._lint(linter, options.input);
 
     const filtered = warnings.filter((w) => !filter.shouldIgnore(w));
 
-    // TODO: Make verbosity and color configurable.
     const printer = new WarningPrinter(
         process.stdout, {analyzer: analyzer, verbosity: 'full', color: true});
     await printer.printWarnings(filtered);
 
     process.exitCode = this._getExitCode(filtered);
+  }
+
+  private async _lint(
+      linter: lintLibTypeOnly.Linter,
+      input: string[]|undefined): Promise<Warning[]> {
+    if (input) {
+      return linter.lint(input);
+    } else {
+      return linter.lintPackage();
+    }
   }
 
   private _getExitCode(filteredWarnings: Warning[]) {
