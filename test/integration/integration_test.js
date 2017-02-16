b10a7c9c9e1eccc2d5199b99c977604f40823f89
Fred K. Schott <fschott@google.com>
2017-02-16 12:45:47 -0800

    put current integration tests into a suite

--- test/integration/integration_test.js
+++ test/integration/integration_test.js
@@ -13,7 +13,6 @@ const path = require('path');
 const runGenerator = require('yeoman-test').run;
 const createGithubGenerator
   = require('../../lib/init/github').createGithubGenerator;
-
 const runCommand = require('./run-command').runCommand;
 
 // Template Generators
@@ -25,16 +24,6 @@ const createElementGenerator
 // A zero priveledge github token of a nonce account, used for quota.
 const githubToken = '8d8622bf09bb1d85cb411b5e475a35e742a7ce35';
 
-const ShopGenerator = createGithubGenerator({
-  owner: 'Polymer',
-  repo: 'shop',
-  githubToken,
-});
-const PSKGenerator = createGithubGenerator({
-  owner: 'PolymerElements',
-  repo: 'polymer-starter-kit',
-  githubToken,
-});
 
 suite('integration tests', function() {
 
@@ -43,75 +32,91 @@ suite('integration tests', function() {
   // Extend timeout limit to 90 seconds for slower systems
   this.timeout(90000);
 
-  // TODO(#562): enable test commands.
-  // TODO(rictic): run linter once new linter lands.
-  test('test the polymer 1.x application template', () => {
-    let dir;
-    return runGenerator(createApplicationGenerator('polymer-1.x'))
-      .withPrompts({ name: 'my-app' }) // Mock the prompt answers
-      .toPromise()
-      .then((_dir) => { dir = _dir })
-      .then(() => runCommand(binPath, ['install'], {cwd: dir}))
-      // .then(() => runCommand(binPath, ['lint'], {cwd: dir}))
-      // .then(() => runCommand(binPath, ['test'], {cwd: dir}))
-      .then(() => runCommand(binPath, ['build'], {cwd: dir}));
-  });
+  suite('init templates', () => {
 
-  test('test the polymer 2.x application template', () => {
-    let dir;
-    return runGenerator(createApplicationGenerator('polymer-2.x'))
-      .withPrompts({ name: 'my-app' }) // Mock the prompt answers
-      .toPromise()
-      .then((_dir) => { dir = _dir })
-      .then(() => runCommand(binPath, ['install'], { cwd: dir }))
-      // .then(() => runCommand(binPath, ['lint'], {cwd: dir}))
-      // .then(() => runCommand(binPath, ['test'], {cwd: dir}))
-      .then(() => runCommand(binPath, ['build'], {cwd: dir}));
-  });
+    // TODO(#562): enable test commands.
+    // TODO(rictic): run linter once new linter lands.
+    test('test the polymer 1.x application template', () => {
+      let dir;
+      return runGenerator(createApplicationGenerator('polymer-1.x'))
+        .withPrompts({ name: 'my-app' }) // Mock the prompt answers
+        .toPromise()
+        .then((_dir) => { dir = _dir })
+        .then(() => runCommand(binPath, ['install'], {cwd: dir}))
+        // .then(() => runCommand(binPath, ['lint'], {cwd: dir}))
+        // .then(() => runCommand(binPath, ['test'], {cwd: dir}))
+        .then(() => runCommand(binPath, ['build'], {cwd: dir}));
+    });
 
-  test('test the polymer 2.x "element" template', () => {
-    let dir;
-    return runGenerator(createElementGenerator('polymer-2.x'))
-      .withPrompts({ name: 'my-element' }) // Mock the prompt answers
-      .toPromise()
-      .then((_dir) => { dir = _dir })
-      .then(() => runCommand(binPath, ['install'], { cwd: dir }))
-      // .then(() => runCommand(binPath, ['lint'], {cwd: dir}))
-      // .then(() => runCommand(binPath, ['test'], {cwd: dir}));
-  });
+    test('test the polymer 2.x application template', () => {
+      let dir;
+      return runGenerator(createApplicationGenerator('polymer-2.x'))
+        .withPrompts({ name: 'my-app' }) // Mock the prompt answers
+        .toPromise()
+        .then((_dir) => { dir = _dir })
+        .then(() => runCommand(binPath, ['install'], { cwd: dir }))
+        // .then(() => runCommand(binPath, ['lint'], {cwd: dir}))
+        // .then(() => runCommand(binPath, ['test'], {cwd: dir}))
+        .then(() => runCommand(binPath, ['build'], {cwd: dir}));
+    });
 
-  test('test the polymer 1.x "element" template', () => {
-    let dir;
-    return runGenerator(createElementGenerator('polymer-1.x'))
-      .withPrompts({ name: 'my-element' }) // Mock the prompt answers
-      .toPromise()
-      .then((_dir) => { dir = _dir })
-      .then(() => runCommand(binPath, ['install'], { cwd: dir }))
-      // TODO(rictic): reenable with new linter.
-      // .then(() => runCommand(binPath, ['lint'], {cwd: dir}))
-      // .then(() => runCommand(binPath, ['test'], {cwd: dir}));
-  });
+    test('test the polymer 2.x "element" template', () => {
+      let dir;
+      return runGenerator(createElementGenerator('polymer-2.x'))
+        .withPrompts({ name: 'my-element' }) // Mock the prompt answers
+        .toPromise()
+        .then((_dir) => { dir = _dir })
+        .then(() => runCommand(binPath, ['install'], { cwd: dir }))
+        // .then(() => runCommand(binPath, ['lint'], {cwd: dir}))
+        // .then(() => runCommand(binPath, ['test'], {cwd: dir}));
+    });
 
-  test('test the "shop" template', () => {
-    let dir;
-    return runGenerator(ShopGenerator)
-      .toPromise()
-      .then((_dir) => { dir = _dir })
-      .then(() => runCommand(binPath, ['install'], {cwd: dir}))
-      // .then(() => runCommand(binPath, ['lint'], {cwd: dir}))
-      // .then(() => runCommand(binPath, ['test'], {cwd: dir}))
-      .then(() => runCommand(binPath, ['build'], {cwd: dir}));
-  });
+    test('test the polymer 1.x "element" template', () => {
+      let dir;
+      return runGenerator(createElementGenerator('polymer-1.x'))
+        .withPrompts({ name: 'my-element' }) // Mock the prompt answers
+        .toPromise()
+        .then((_dir) => { dir = _dir })
+        .then(() => runCommand(binPath, ['install'], { cwd: dir }))
+        // TODO(rictic): reenable with new linter.
+        // .then(() => runCommand(binPath, ['lint'], {cwd: dir}))
+        // .then(() => runCommand(binPath, ['test'], {cwd: dir}));
+    });
 
-  test('test the "starter-kit" template', () => {
-    let dir;
-    return runGenerator(PSKGenerator)
-      .toPromise()
-      .then((_dir) => { dir = _dir })
-      .then(() => runCommand(binPath, ['install'], { cwd: dir }))
-      // .then(() => runCommand(binPath, ['lint'], {cwd: dir}))
-      // .then(() => runCommand(binPath, ['test'], {cwd: dir}))
-      .then(() => runCommand(binPath, ['build'], {cwd: dir}));
+    test('test the "shop" template', () => {
+      let dir;
+      const ShopGenerator = createGithubGenerator({
+        owner: 'Polymer',
+        repo: 'shop',
+        githubToken,
+      });
+
+      return runGenerator(ShopGenerator)
+        .toPromise()
+        .then((_dir) => { dir = _dir })
+        .then(() => runCommand(binPath, ['install'], {cwd: dir}))
+        // .then(() => runCommand(binPath, ['lint'], {cwd: dir}))
+        // .then(() => runCommand(binPath, ['test'], {cwd: dir}))
+        .then(() => runCommand(binPath, ['build'], {cwd: dir}));
+    });
+
+    test('test the "starter-kit" template', () => {
+      let dir;
+      const PSKGenerator = createGithubGenerator({
+        owner: 'PolymerElements',
+        repo: 'polymer-starter-kit',
+        githubToken,
+      });
+
+      return runGenerator(PSKGenerator)
+        .toPromise()
+        .then((_dir) => { dir = _dir })
+        .then(() => runCommand(binPath, ['install'], { cwd: dir }))
+        // .then(() => runCommand(binPath, ['lint'], {cwd: dir}))
+        // .then(() => runCommand(binPath, ['test'], {cwd: dir}))
+        .then(() => runCommand(binPath, ['build'], {cwd: dir}));
+    });
+
   });
 
 });
