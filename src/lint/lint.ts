8c2fde41cddb9d2d36b4d5f0ed98f155fec985ce
Michael Stramel <stramel@users.noreply.github.com>
2017-05-02 02:18:40 -0500

    Prevent Warnings from erroring
    Quick fix to prevent linter from erroring out when warnings or info are present. Ideally would be configurable

--- src/lint/lint.ts
+++ src/lint/lint.ts
@@ -84,6 +84,6 @@ export async function lint(options: Options, config: ProjectConfig) {
       message += ` ${infos.length} ${chalk.green('info')} messages`;
     }
     console.log(`\n\nFound ${message}.`);
-    return new CommandResult(1);
+    return new CommandResult(errors.length ? 1 : 0);
   }
 }
